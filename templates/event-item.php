74c393d9f5ca69b3b5f311986a8d20dcbb658f24
geodefender <119744905+geodefender@users.noreply.github.com>
2026-02-27 09:07:06 -0300

    Escape event URLs in template links

--- templates/event-item.php
+++ templates/event-item.php
@@ -8,12 +8,12 @@
         <div class="cal-google-event-description"><?php echo esc_html($event['description']); ?></div>
     <?php endif; ?>
     <?php if ($event['event_url'] !== '') : ?>
-        <div class="cal-google-event-description"><a href="<?php echo $event['event_url']; ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['event_link_label']); ?></a></div>
+        <div class="cal-google-event-description"><a href="<?php echo esc_url($event['event_url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['event_link_label']); ?></a></div>
     <?php endif; ?>
     <?php if ($event['google_template_url'] !== '') : ?>
-        <div class="cal-google-event-description"><a href="<?php echo $event['google_template_url']; ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['google_template_link_label']); ?></a></div>
+        <div class="cal-google-event-description"><a href="<?php echo esc_url($event['google_template_url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['google_template_link_label']); ?></a></div>
     <?php endif; ?>
     <?php if ($event['ics_download_url'] !== '') : ?>
-        <div class="cal-google-event-description"><a href="<?php echo $event['ics_download_url']; ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['download_ics_label']); ?></a></div>
+        <div class="cal-google-event-description"><a href="<?php echo esc_url($event['ics_download_url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($event['download_ics_label']); ?></a></div>
     <?php endif; ?>
 </article>
